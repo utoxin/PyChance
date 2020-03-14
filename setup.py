74e3f84cdcd717e02ea2b3698c370220db6f6704
Matthew Walker <utoxin@gmail.com>
2020-03-14 15:10:06 -0600

    Tweaking things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='PyChance',
-        version='0.1.7',
+        version='0.1.8',
         packages=find_packages(),
         url='https://github.com/utoxin/PyChance',
         license='MIT',
