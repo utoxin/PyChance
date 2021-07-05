a6cb69a01bbb85795fd5d0505bf8d0a007f4bc54
Matthew Walker <utoxin@gmail.com>
2021-07-05 11:10:21 -0600

    Committing old work
    Prepping to resume work on this.

--- pychance/pychance_class.py
+++ pychance/pychance_class.py
@@ -13,9 +13,11 @@ class PyChance:
 if __name__ == '__main__':
     from pychance.data import SimpleTable
 
-    test_table = SimpleTable("foo", ["bar", "baz"])
+    test_table = SimpleTable("foo", ["[bar]", "baz"])
+    test_table2 = SimpleTable("bar", ["testing", "this"])
 
     test = PyChance()
     test.add_table("foo", test_table)
+    test.add_table("bar", test_table2)
     print(test.parser.parse("This is a [foo] test."))
 
